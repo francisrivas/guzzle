4e740ca8a398ad8b255206889e6a7a035b89f8da
francisrivas <145857443+francisrivas@users.noreply.github.com>
2024-08-02 14:13:08 -0600

    Update CurlMultiHandler.php

--- src/Handler/CurlMultiHandler.php
+++ src/Handler/CurlMultiHandler.php
@@ -70,14 +70,15 @@ class CurlMultiHandler
     {
         $this->factory = $options['handle_factory'] ?? new CurlFactory(50);
 
-        if (isset($options['select_timeout'])) {
-            $this->selectTimeout = $options['select_timeout'];
-        } elseif ($selectTimeout = Utils::getenv('GUZZLE_CURL_SELECT_TIMEOUT')) {
-            @trigger_error('Since guzzlehttp/guzzle 7.2.0: Using environment variable GUZZLE_CURL_SELECT_TIMEOUT is deprecated. Use option "select_timeout" instead.', \E_USER_DEPRECATED);
-            $this->selectTimeout = (int) $selectTimeout;
-        } else {
-            $this->selectTimeout = 1;
-        }
+        if ($selectTimeout = Utils::getenv('GUZZLE_CURL_SELECT_TIMEOUT')) {
+    if (version_compare(GUZZLE_VERSION, '7.2.0', '>=')) {
+        trigger_error('Using environment variable GUZZLE_CURL_SELECT_TIMEOUT is deprecated. Use option "select_timeout" instead.', \E_USER_DEPRECATED);
+    }
+    $this->selectTimeout = (int) $selectTimeout;
+} else {
+    $this->selectTimeout = 1;
+}
+
 
         $this->options = $options['options'] ?? [];
 
